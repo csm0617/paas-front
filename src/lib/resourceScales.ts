8bff47261ca4d2b7eccbc564dfa9b06608ed991a
csm <43975255@qq.com>
2026-05-17 09:46:48 +0800

    refactor: main分支仅保留v1，vNext内容移至vnext分支

--- src/lib/resourceScales.ts
+++
@@ -1,6 +0,0 @@
-export type CpuUnit = 'm' | 'c'
-export type MemoryUnit = 'Mi' | 'Gi'
-
-export const CPU_MILLICORE_STOPS = [50, 100, 250, 500, 1000, 2000, 4000] as const
-export const MEMORY_MI_STOPS = [64, 128, 256, 512, 1024, 2048, 4096] as const
-
